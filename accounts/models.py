875fa653f500dd6cbba7f450e57707b3d3d3a58a
KamilPaczoski <kamil.paczoski13@gmail.com>
2024-06-21 03:18:22 +0200

    Fixed issue with custom avatars - wrongly declared path to media file in settings.py current issue: avatar eiditon

--- accounts/models.py
+++ accounts/models.py
@@ -1,24 +1,17 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+import uuid
+import os
+
+
+def user_avatar_path(instance, filename):
+    # Generate a random unique token
+    ext = filename.split('.')[-1]
+    filename = f'{uuid.uuid4()}.{ext}'
+    return os.path.join('user_avatars', filename)
 
 
 class CustomUser(AbstractUser):
-    avatar = models.ImageField(upload_to='../media/user_avatars/', null=True, blank=True,
-                               default='../static/images/default_avatar.webp')
+    avatar = models.ImageField(upload_to=user_avatar_path, null=True, blank=True, default='images/default_avatar.webp')
     bio = models.TextField(null=True, blank=True)
     email = models.EmailField(unique=True)
-    # groups = models.ManyToManyField(
-    #     'auth.Group',
-    #     related_name='customuser_set',  # Unique related_name for CustomUser
-    #     blank=True,
-    #     help_text=('The groups this user belongs to. A user will get all permissions '
-    #                'granted to each of their groups.'),
-    #     related_query_name='customuser',
-    # )
-    # user_permissions = models.ManyToManyField(
-    #     'auth.Permission',
-    #     related_name='customuser_set',  # Unique related_name for CustomUser
-    #     blank=True,
-    #     help_text='Specific permissions for this user.',
-    #     related_query_name='customuser',
-    # )
--- accounts/views.py
+++ accounts/views.py
@@ -2,34 +2,11 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, update_session_auth_hash
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.decorators import login_required
-from django import forms
 from .models import CustomUser
-from .forms import UserUpdateForm, CustomPasswordChangeForm
-from django.contrib.auth import logout
+from .forms import UserUpdateForm, CustomPasswordChangeForm, CustomUserCreationForm
 
 
-class RegistrationForm(forms.ModelForm):
-    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput)
-    avatar = forms.ImageField(required=False)
-
-    class Meta:
-        model = CustomUser
-        fields = ['username', 'email', 'avatar']
-
-    def clean_password2(self):
-        cd = self.cleaned_data
-        if cd['password1'] != cd['password2']:
-            raise forms.ValidationError('Passwords don’t match.')
-        return cd['password2']
-
-    def save(self, commit=True):
-        user = super().save(commit=False)
-        user.set_password(self.cleaned_data['password1'])
-        if commit:
-            user.save()
-        return user
-
+# todo: fix issue with editing by user his avatar on account settings page
 
 def login_register(request):
     if request.method == 'POST':
@@ -47,13 +24,16 @@ def login_register(request):
                     else:
                         return redirect('user_shelf')
         elif 'register' in request.POST:
-            registration_form = RegistrationForm(request.POST, request.FILES)
+            registration_form = CustomUserCreationForm(request.POST, request.FILES)
             if registration_form.is_valid():
-                registration_form.save()
+                user = registration_form.save(commit=False)
+                if 'avatar' in request.FILES:
+                    user.avatar = request.FILES['avatar']
+                user.save()
                 return redirect('login_register')
     else:
         login_form = AuthenticationForm()
-        registration_form = RegistrationForm()
+        registration_form = CustomUserCreationForm()
 
     return render(request, 'login_register.html', {
         'login_form': login_form,
@@ -94,4 +74,3 @@ def delete_account(request):
     if request.method == 'POST':
         request.user.delete()
         return redirect('login_register')
-    return render(request, 'account_settings.html')
